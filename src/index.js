94a3758a63361f7b58417c5e8a48f2dc4ecd2479
Lauren Boyer <lboyer4@outlook.com>
2019-04-16 21:12:32 -0600

    added snapshot in card holder test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './SCSS/index.css';
+import './SCSS/index.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
